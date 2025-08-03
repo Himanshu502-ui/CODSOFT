175a41cb359885c620bc2af56055677cf438030a
Himanshu502-ui <himanshujatav500@gmail.com>
2025-08-03 20:18:30 +0530

    Update task_1.cpp

--- task_1.cpp
+++ task_1.cpp
@@ -1,6 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"harsh";
-}
 
+int main() {
+    double a, b;
+    char opera;
+
+    cout<< "Welcome to simple calculator "<<endl <<endl;
+
+   cout <<"Enter the first number = ";
+    cin >> a;
+
+     cout <<"Enter the second numbr = ";
+    cin >> b;
+
+
+     cout << "Enter the operator (+, -, *, /) = ";
+    cin >>opera;
+
+   
+    switch (opera) {
+        case '+':
+            cout << "Addition: " << a + b << endl;
+            break;
+        case '-':
+            cout << "Subtraction: " << a - b << endl;
+            break;
+     case '*':
+            cout << "Multiplication: " << a * b << endl;
+     break;
+        case '/':
+            if (b != 0)
+                cout << " Division: " << a / b << endl;
+            else
+                cout << "Error! Division by zero is not alowed" << endl;
+            break;
+        default:
+            cout << "you entered the wrong operator! Please use +, -, *, or /" << endl;
+    }
+
+    return 0;
+}
